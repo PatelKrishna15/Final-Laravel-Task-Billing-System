10fa773a87ba7a16d8224a61dff4a6465c08ad06
hk pate <krishahirapara666@gmail.com>
2023-06-15 11:10:18 +0530

    create

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -25,4 +25,8 @@ class Company extends Model
     {
         return $this->belongsTo(Country::class,'country_id');
     }
+    public function product()
+    {
+        return $this->belongsTo(Product::class,'name');
+    }
 }
